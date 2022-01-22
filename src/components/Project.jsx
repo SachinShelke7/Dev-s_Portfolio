a673933e75e0b448f1490f88795aa2c3a80b69d0
Sachin Shelke <shelkesp1@gmail.com>
2022-01-22 18:43:34 +0530

    added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,29 +3,32 @@ import StoryCard from './StoryCard.jsx'
 
 const projects = [
     {
-      name: 'Sachin Shelke',
-      src: '',
-      detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ratione cum magnam temporibus corrupti asperiores provident, fugit molestias quos sunt, tempore sequi nihil iste fugiat consequatur cupiditate rem officiis harum a vitae praesentium ab!',
+      name: 'Facebook Clone',
+      src: 'https://github.com/SachinShelke7/Dev-s_Portfolio/blob/main/src/assets/images/fb.png?raw=true',
+      tech: 'IN REACT.JS, NEXT.JS, TAILWIND CSS & FIREBASE, FIRESTORE',
+      detail: 'I have build Facebook clone with ReactJS, NextJS, Tailwind CSS also Next-Auth for Facebook and Google authentication.It has all sharing functionality like facebook.',
+      link: 'https://facebook-clone-psi-seven.vercel.app/'
     },
     {
-      name: 'Prati Shinde',
-      src: 'https://images.unsplash.com/photo-1607992922515-7e38329e65d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=714&q=80',
-      detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ratione cum magnam temporibus corrupti asperiores provident, fugit molestias quos sunt, tempore sequi nihil iste fugiat consequatur cupiditate rem officiis harum a vitae praesentium ab!',
+      name: 'Amazon Clone',
+      src: 'https://github.com/SachinShelke7/Dev-s_Portfolio/blob/main/src/assets/images/amazon.png?raw=true',
+      tech: 'IN REACT.JS, TAILWIND CSS, COMMERCE JS.',
+      detail: 'I have build Amazon clone with ReactJS, Tailwind CSS and Commerce JS as a backend.',
+      link: 'https://e-com-feature.vercel.app/'
     },
     {
-      name: 'Ranjeet Harishchandre',
-      src: 'https://images.unsplash.com/photo-1618886487325-f665032b6352?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
-      detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ratione cum magnam temporibus corrupti asperiores provident, fugit molestias quos sunt, tempore sequi nihil iste fugiat consequatur cupiditate rem officiis harum a vitae praesentium ab!',
+      name: 'Google Clone',
+      src: 'https://github.com/SachinShelke7/Dev-s_Portfolio/blob/main/src/assets/images/google.png?raw=true',
+      tech: 'IN REACT.JS, NEXT.JS, TAILWIND CSS & GOOGLE SEARCH API',
+      detail: 'I have build Google Search clone with ReactJS, NextJS & Tailwind CSS also Google search APIs for Search functionality. It has all sharing functionality like .',
+      link: 'https://google2-0-tan.vercel.app/'
     },
     {
-      name: 'Abhi Shinde',
-      src: 'https://images.unsplash.com/photo-1518495973542-4542c06a5843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ratione cum magnam temporibus corrupti asperiores provident, fugit molestias quos sunt, tempore sequi nihil iste fugiat consequatur cupiditate rem officiis harum a vitae praesentium ab!',
-    },
-    {
-      name: 'Sagar Borude',
-      src: 'https://images.unsplash.com/photo-1585016495481-91613a3ab1bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
-      detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ratione cum magnam temporibus corrupti asperiores provident, fugit molestias quos sunt, tempore sequi nihil iste fugiat consequatur cupiditate rem officiis harum a vitae praesentium ab!',
+      name: 'Hulu Clone',
+      src: 'https://github.com/SachinShelke7/Dev-s_Portfolio/blob/main/src/assets/images/hulu.png?raw=true',
+      tech: 'IN REACT.JS, NEXT.JS & TAILWIND CSS, TMDB API.',
+      detail: 'I have build HULU clone in JavaScript using ReactJS, NextJS, Tailwind CSS and live TMDB APIs.It has filter functionality according genres movie type etc. ',
+      link: 'https://hulu2-0-rosy.vercel.app/'
     },
   ];
 
@@ -34,7 +37,7 @@ const Project = () => {
   return <div>
       <div className='flex flex-col justify-center mx-auto w-full px-32'>
             {projects.map((project) => (
-                <StoryCard key={project.name} name={project.name} src={project.src} detail={project.detail}/>
+                <StoryCard key={project.name} name={project.name} src={project.src} detail={project.detail} tech={project.tech} link={project.link}/>
             ))}
         </div>
   </div>;
--- src/components/StoryCard.jsx
+++ src/components/StoryCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-function StoryCard({ name, src, detail }) {
+function StoryCard({ name, src, detail, tech, link }) {
     return (
-        <div className='w-full px-5 lg:px-0 pt-28 flex'>
+        <div className='w-full px-5 lg:px-0 flex bg-gray-100 shadow-lg mt-28 rounded-lg'>
             <img 
-            className="object-cover w-[400px] h-auto"
+            className="object-cover w-[400px] h-auto rounded-l-lg"
             src={src}
             layout="fill"
             alt='ImageP'
             />
             <div className='pl-10'>
-            <p className='w-5/6 text-black text-sm font-bold'>{name}</p>
-            <p>{detail}</p>
+            <p className='w-5/6 text-black text-xl font-bold font-serif'>{name}</p>
+            <p className='text-lg font-medium'>{tech}</p>
+            <p className='pb-5 text-gray-600'>{detail}</p>
+            <a href={link} target="_blank" className='bg-gray-300 px-4 py-2 rounded-md hover:text-white cursor-pointer hover:bg-gray-900 transition-colors duration-500'>Live Link</a>
             </div>
 
         </div>
